0fe36db1e9c0566ee66e3db8ee207135b7f758c7
tulio.resende <tulio.resende@dtidigital.com.br>
2022-02-11 15:29:41 -0300

    add style bases to topicCard

--- src/features/topicsView/cards/TopicCard.tsx
+++ src/features/topicsView/cards/TopicCard.tsx
@@ -1,4 +1,4 @@
-import { Card } from "@mui/material";
+import { Card, IconButton, Typography } from "@mui/material";
 import { FunctionComponent } from "react";
 import { ITopic } from "../api/TopicsViewApiInterface";
 
@@ -11,10 +11,36 @@ export const TopicCard: FunctionComponent<topicCardEntries> = ({
 }) => {
   const { name } = topicInfo;
   return (
-    <Card style={{padding: 8, margin: 8}}>
-      {name}
+    <Card style={styles.root}>
+      <div>
+        <Typography>
+          {name}
+        </Typography>
+        <Typography>
+          {'28/30'}
+        </Typography>
+      </div>
+      <div>
+        <Typography>
+          <IconButton>
+            {'A'}
+          </IconButton>
+        </Typography> 
+      </div>
     </Card>
   );
 };
+
+const styles = {
+  root: {
+    padding: 8, 
+    margin: 8, 
+    width: 180,
+    display: "flex",
+    displayDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center"
+  }
+}
 
 export default TopicCard;
---
+++ src/shared/components/text/simpleText/SimpleText.tsx
@@ -0,0 +1,10 @@
+import { Typography } from "@mui/material";
+
+function SimpleText(){
+    return 
+    <Typography>
+
+    </Typography>
+} 
+
+export default SimpleText;
